2b4123ecb899080357743199b356c7e7aa9362ce
Yurlov <Yurlov@lanit.ru>
2024-11-06 18:13:34 +0300

    EDU-38398    Формирование данных для системы учёта рабочего времени

--- src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/FindVacationsToApproveUseCase.java
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/FindVacationsToApproveUseCase.java
@@ -1,17 +1,30 @@
 package ru.lanit.bpm.jedu.hrjedi.app.impl.vacation;
 
 import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.task.Task;
 import org.springframework.stereotype.Component;
 import ru.lanit.bpm.jedu.hrjedi.app.api.vacation.FindVacationsToApproveInbound;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class FindVacationsToApproveUseCase implements FindVacationsToApproveInbound {
     @Override
     public Set<String> execute(String approverLogin) {
-        return Collections.emptySet();
+        try {
+            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+            TaskService taskService = processEngine.getTaskService();
+            List<Task> tasks = taskService.createTaskQuery().processVariableValueEquals("approverLogin", approverLogin).active().list();
+            return tasks.stream().map(Task::getProcessInstanceId).collect(Collectors.toSet());
+        } catch (Exception e) {
+            return Collections.emptySet();
+        }
     }
 }
