77454f25065ae4b6090a552f8e347b76b1a74538
Yurlov <Yurlov@lanit.ru>
2024-11-11 13:56:24 +0300

    EDU-38390    Добавить сущность "Рабочее место" к текущей модели данных

---
+++ src/main/resources/db/migration/0101/V3__added_work_space_entity.sql
@@ -0,0 +1,22 @@
+CREATE TABLE WORKSPACE
+(
+    id           NUMBER PRIMARY KEY,
+    office_id    NUMBER NOT NULL REFERENCES OFFICE (id),
+    floor        NUMBER NOT NULL,
+    room         NUMBER NOT NULL,
+    table_number NUMBER NOT NULL,
+    employee_id  NUMBER REFERENCES EMPLOYEE (id)
+);
+CREATE SEQUENCE sq_workspace_id START WITH 1 INCREMENT BY 1;
+
+INSERT INTO WORKSPACE
+VALUES (next value for sq_workspace_id, 2, 7, 711, 7, 5),
+       (next value for sq_workspace_id, 2, 7, 711, 8, null),
+       (next value for sq_workspace_id, 2, 7, 711, 9, null),
+       (next value for sq_workspace_id, 2, 7, 711, 10, null),
+       (next value for sq_workspace_id, 2, 7, 711, 11, null),
+       (next value for sq_workspace_id, 1, 3, 324, 1, 6),
+       (next value for sq_workspace_id, 1, 3, 324, 2, 7),
+       (next value for sq_workspace_id, 1, 3, 324, 3, null),
+       (next value for sq_workspace_id, 1, 3, 324, 4, null),
+       (next value for sq_workspace_id, 1, 3, 324, 5, null);
