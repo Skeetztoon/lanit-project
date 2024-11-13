9b2b4c1de47a489ec539f90db22c69b668ce2461
Yulov <Yulov@lanit.ru>
2024-11-13 17:25:15 +0300

    EDU-38397    Реализовать получение информации о пользователях Добавлены unit-тесты для usecase'ов

---
+++ src/test/java/ru/lanit/bpm/jedu/hrjedi/app/impl/employee/GetRolesQuantityTest.java
@@ -0,0 +1,46 @@
+package ru.lanit.bpm.jedu.hrjedi.app.impl.employee;
+
+import camundajar.impl.scala.math.BigInt;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.EmployeeRepository;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetRolesQuantityTest {
+
+    private final static String ROLE = "ROLE";
+    private final static String USERS_QUANTITY = "USERS_QUANTITY";
+
+    @Mock
+    EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    GetRolesQuantityUseCase getRolesQuantityUseCase;
+
+    @Test
+    public void getRolesQuantity_singleRole() {
+        List<Map<String, Object>> roles = new ArrayList<>();
+        roles.add(Map.of(ROLE, "admin", USERS_QUANTITY, BigInteger.valueOf(1)));
+        roles.add(Map.of(ROLE, "hr", USERS_QUANTITY, BigInteger.valueOf(2)));
+        roles.add(Map.of(ROLE, "user", USERS_QUANTITY, BigInteger.valueOf(10)));
+        Map<String, Long> expectedOutput = new HashMap<>();
+        expectedOutput.put("admin",  1L);
+        expectedOutput.put("hr", 2L);
+        expectedOutput.put("user", 10L);
+
+        Mockito.when(employeeRepository.getRolesQuantity()).thenReturn(roles);
+
+        Assert.assertEquals(expectedOutput, getRolesQuantityUseCase.execute());
+    }
+}
---
+++ src/test/java/ru/lanit/bpm/jedu/hrjedi/app/impl/employee/GetUsersWithRolesTest.java
@@ -0,0 +1,65 @@
+package ru.lanit.bpm.jedu.hrjedi.app.impl.employee;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.EmployeeRepository;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static java.util.Arrays.asList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetUsersWithRolesTest {
+
+    private final static String LOGIN = "login";
+    private final static String ROLES = "roles";
+    private final static String ADMIN = "Admin";
+    private final static String HR = "Hr";
+    private final static String USER = "User";
+
+    @Mock
+    EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    GetUsersWithRolesUseCase getUsersWithRolesUseCase;
+
+    @Test
+    public void getRolesQuantity_singleRole() {
+        List<Map<String, Object>> roles = new ArrayList<>();
+        roles.add(Map.of(LOGIN, "user1", ROLES, ADMIN));
+        roles.add(Map.of(LOGIN, "user2", ROLES, HR));
+        roles.add(Map.of(LOGIN, "user3", ROLES, USER));
+        List<Map<String, Object>> expectedOutput = new ArrayList<>();
+        expectedOutput.add(Map.of(LOGIN, "user1", ROLES, asList(Map.of("name", ADMIN))));
+        expectedOutput.add(Map.of(LOGIN, "user2", ROLES, asList(Map.of("name", HR))));
+        expectedOutput.add(Map.of(LOGIN, "user3", ROLES, asList(Map.of("name", USER))));
+
+        Mockito.when(employeeRepository.getUsersWithRoles()).thenReturn(roles);
+
+        Assert.assertEquals(expectedOutput, getUsersWithRolesUseCase.execute());
+    }
+
+    @Test
+    public void getRolesQuantity_multipleRoles() {
+        List<Map<String, Object>> roles = new ArrayList<>();
+        roles.add(Map.of(LOGIN, "user1", ROLES, ADMIN+" "+HR));
+        roles.add(Map.of(LOGIN, "user2", ROLES, HR));
+        roles.add(Map.of(LOGIN, "user3", ROLES, USER));
+        List<Map<String, Object>> expectedOutput = new ArrayList<>();
+        expectedOutput.add(Map.of(LOGIN, "user1", ROLES, asList(Map.of("name", ADMIN), Map.of("name", HR))));
+        expectedOutput.add(Map.of(LOGIN, "user2", ROLES, asList(Map.of("name", HR))));
+        expectedOutput.add(Map.of(LOGIN, "user3", ROLES, asList(Map.of("name", USER))));
+
+        Mockito.when(employeeRepository.getUsersWithRoles()).thenReturn(roles);
+
+        Assert.assertEquals(expectedOutput, getUsersWithRolesUseCase.execute());
+    }
+}
