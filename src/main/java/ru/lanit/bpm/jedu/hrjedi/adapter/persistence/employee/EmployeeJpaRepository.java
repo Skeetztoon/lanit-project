cb5b83adbafbf0f1beb2ced480a823d9a1dc81db
Yurlov <Yurlov@lanit.ru>
2024-11-18 10:13:09 +0300

    EDU-38397    Реализовать получение информации о пользователях

--- src/main/java/ru/lanit/bpm/jedu/hrjedi/adapter/persistence/employee/EmployeeJpaRepository.java
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/adapter/persistence/employee/EmployeeJpaRepository.java
@@ -17,7 +17,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.lanit.bpm.jedu.hrjedi.domain.employee.Employee;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByLoginIgnoreCase(String login);
