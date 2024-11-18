089beaf752ce7a67a0438db4ed4e80873e976be8
Yurlov <Yurlov@lanit.ru>
2024-11-18 10:21:40 +0300

    EDU-38391    Добавить функцию загрузки аватарок

--- src/main/java/ru/lanit/bpm/jedu/hrjedi/adapter/rest/employee/EmployeeController.java
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/adapter/rest/employee/EmployeeController.java
@@ -19,11 +19,27 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import ru.lanit.bpm.jedu.hrjedi.adapter.rest.employee.dto.SignUpFormDto;
-import ru.lanit.bpm.jedu.hrjedi.app.api.employee.*;
+
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.CreateEmployeeInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.FindAllEmployeesInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.FindEmployeeByLoginInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.UpdateEmployeeEmailInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.GetEmployeeFullNameByLoginInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.GetEmployeeAvatarInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.SetEmployeeAvatarInbound;
 import ru.lanit.bpm.jedu.hrjedi.app.api.security.GenerateSecurePasswordInbound;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.EmployeeRegistrationException;
 import ru.lanit.bpm.jedu.hrjedi.domain.employee.Employee;
 import ru.lanit.bpm.jedu.hrjedi.domain.employee.EmployeeAvatar;
 
@@ -112,7 +128,7 @@ public class EmployeeController {
         if (isSaved) {
             return ResponseEntity.ok("File saved successfully");
         } else {
-            return ResponseEntity.ok().body("Error saving file");
+            return ResponseEntity.badRequest().body("Error saving file");
         }
     }
 
