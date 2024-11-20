174497be5a0a03b03ff9dd9762c78b9b5ebd4b7b
Yurlov <Yurlov@lanit.ru>
2024-11-20 10:55:51 +0300

    EDU-38398    Формирование данных для системы учёта рабочего времени

--- src/main/java/ru/lanit/bpm/jedu/hrjedi/app/api/vacation/FindVacationsToApproveException.java
+++
@@ -1,7 +0,0 @@
-package ru.lanit.bpm.jedu.hrjedi.app.api.vacation;
-
-public class FindVacationsToApproveException extends RuntimeException {
-    public FindVacationsToApproveException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-}
--- src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/FindVacationsToApproveUseCase.java
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/FindVacationsToApproveUseCase.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.task.Task;
 import org.springframework.stereotype.Component;
-import ru.lanit.bpm.jedu.hrjedi.app.api.vacation.FindVacationsToApproveException;
 import ru.lanit.bpm.jedu.hrjedi.app.api.vacation.FindVacationsToApproveInbound;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,11 +17,13 @@ public class FindVacationsToApproveUseCase implements FindVacationsToApproveInbo
 
     @Override
     public Set<String> execute(String approverLogin) {
-        try {
-            List<Task> tasks = taskService.createTaskQuery().processVariableValueEquals("approverLogin", approverLogin).active().list();
-            return tasks.stream().map(Task::getProcessInstanceId).collect(Collectors.toSet());
-        } catch (Exception e) {
-            throw new FindVacationsToApproveException("Error finding vacations", e);
-        }
+        return taskService
+            .createTaskQuery().
+            processVariableValueEquals("approverLogin", approverLogin)
+            .active()
+            .list()
+            .stream()
+            .map(Task::getProcessInstanceId)
+            .collect(Collectors.toSet());
     }
 }
