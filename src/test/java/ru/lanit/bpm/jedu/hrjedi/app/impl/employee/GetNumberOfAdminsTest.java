1d76e17e5fd8a184a06533b2b4645d57d1a29d15
Yurlov <Yurlov@lanit.ru>
2024-11-21 13:38:02 +0300

    EDU-38387    Добавить серверную валидацию почтового ящика

---
+++ src/test/java/ru/lanit/bpm/jedu/hrjedi/app/impl/employee/GetNumberOfAdminsTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2008-2020
+ * LANIT
+ * All rights reserved.
+ *
+ * This product and related documentation are protected by copyright and
+ * distributed under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or related documentation may be
+ * reproduced in any form by any means without prior written authorization of
+ * LANIT and its licensors, if any.
+ *
+ * $
+ */
+package ru.lanit.bpm.jedu.hrjedi.app.impl.employee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import ru.lanit.bpm.jedu.hrjedi.app.api.employee.EmployeeRepository;
+import ru.lanit.bpm.jedu.hrjedi.domain.employee.Employee;
+import ru.lanit.bpm.jedu.hrjedi.domain.security.Role;
+import ru.lanit.bpm.jedu.hrjedi.domain.security.RoleName;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
+import static org.junit.Assert.assertEquals;
+import static ru.lanit.bpm.jedu.hrjedi.domain.security.RoleName.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetNumberOfAdminsTest {
+    @Mock
+    EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    GetNumberOfAdmins getNumberOfAdmins;
+
+    @Test
+    public void getNumberOfAdmins() {
+        Mockito.when(employeeRepository.findAll()).thenReturn(asList(
+            user(singleton(role(ROLE_USER))),
+            user(singleton(role(ROLE_OMNI))),
+            user(asList(role(ROLE_USER), role(ROLE_OMNI))),
+            user(asList(role(ROLE_USER), role(ROLE_ADMIN))),
+            user(singleton(role(ROLE_ADMIN)))
+        ));
+
+        long numberOfAdmins = getNumberOfAdmins.execute();
+
+        assertEquals(2, numberOfAdmins);
+    }
+
+    // ===================================================================================================================
+    // = Implementation
+    // ===================================================================================================================
+
+    private Employee user(Collection<Role> roles) {
+        Employee user = new Employee();
+        user.setRoles(new HashSet<>(roles));
+
+        return user;
+    }
+
+    private Role role(RoleName name) {
+        Role role = new Role();
+        role.setName(name);
+
+        return role;
+    }
+}
