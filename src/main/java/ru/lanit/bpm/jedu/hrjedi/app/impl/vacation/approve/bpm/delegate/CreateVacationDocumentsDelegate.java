4824cb81c2775124fd4c9c56c05966c5f89eac0e
Yurlov <Yurlov@lanit.ru>
2024-11-25 14:09:08 +0300

    EDU-38392    Обновление информации в системе бух.учёта: Процесс

--- src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/approve/bpm/delegate/CreateVacationDocumentsDelegate.java
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/approve/bpm/delegate/CreateVacationDocumentsDelegate.java
@@ -3,16 +3,20 @@ package ru.lanit.bpm.jedu.hrjedi.app.impl.vacation.approve.bpm.delegate;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ru.lanit.bpm.jedu.hrjedi.adapter.accounting.CreateVacationDocumentsAdapter;
+import ru.lanit.bpm.jedu.hrjedi.adapter.email.NotifyOnVacationApprovalAdapter;
 import ru.lanit.bpm.jedu.hrjedi.app.api.vacation.CreateVacationDocumentsException;
 import ru.lanit.bpm.jedu.hrjedi.domain.vacation.Vacation;
 
 @Component("createVacationDocumentsDelegate")
 @RequiredArgsConstructor
 public class CreateVacationDocumentsDelegate implements JavaDelegate {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateVacationDocumentsDelegate.class);
     private static final String SENDING_STATUS = "sendingStatus";
-    private static final String DECLINE = "declined";
+    private static final String DECLINE = "decline";
     private static final String ACCEPT = "accept";
 
     private final CreateVacationDocumentsAdapter adapter;
@@ -20,18 +24,18 @@ public class CreateVacationDocumentsDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         Vacation vacation = (Vacation) delegateExecution.getVariable("vacation");
-
+        LOGGER.info("ДЕЛЕГАТ ОТПРАВКИ НАЧАЛО");
         if (vacation != null) {
-            adapter.execute(vacation);
             try {
                 adapter.execute(vacation);
+                delegateExecution.setVariable(SENDING_STATUS, ACCEPT);
             } catch (CreateVacationDocumentsException e) {
                 delegateExecution.setVariable(SENDING_STATUS, DECLINE);
             }
-            delegateExecution.setVariable(SENDING_STATUS, ACCEPT);
         } else {
             delegateExecution.setVariable(SENDING_STATUS, DECLINE);
             throw new IllegalStateException("Vacation is missing");
         }
+        LOGGER.info(delegateExecution.getVariable(SENDING_STATUS).toString());
     }
 }
