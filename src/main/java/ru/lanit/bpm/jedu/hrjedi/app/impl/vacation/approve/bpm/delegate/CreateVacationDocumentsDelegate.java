ce5bd52f087969b90d8c75488a7d7622e03c1ea3
Yurlov <Yurlov@lanit.ru>
2024-11-25 14:09:47 +0300

    EDU-38392    Обновление информации в системе бух.учёта: Процесс

--- src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/approve/bpm/delegate/CreateVacationDocumentsDelegate.java
+++ src/main/java/ru/lanit/bpm/jedu/hrjedi/app/impl/vacation/approve/bpm/delegate/CreateVacationDocumentsDelegate.java
@@ -3,18 +3,14 @@ package ru.lanit.bpm.jedu.hrjedi.app.impl.vacation.approve.bpm.delegate;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ru.lanit.bpm.jedu.hrjedi.adapter.accounting.CreateVacationDocumentsAdapter;
-import ru.lanit.bpm.jedu.hrjedi.adapter.email.NotifyOnVacationApprovalAdapter;
 import ru.lanit.bpm.jedu.hrjedi.app.api.vacation.CreateVacationDocumentsException;
 import ru.lanit.bpm.jedu.hrjedi.domain.vacation.Vacation;
 
 @Component("createVacationDocumentsDelegate")
 @RequiredArgsConstructor
 public class CreateVacationDocumentsDelegate implements JavaDelegate {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CreateVacationDocumentsDelegate.class);
     private static final String SENDING_STATUS = "sendingStatus";
     private static final String DECLINE = "decline";
     private static final String ACCEPT = "accept";
@@ -24,7 +20,6 @@ public class CreateVacationDocumentsDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         Vacation vacation = (Vacation) delegateExecution.getVariable("vacation");
-        LOGGER.info("ДЕЛЕГАТ ОТПРАВКИ НАЧАЛО");
         if (vacation != null) {
             try {
                 adapter.execute(vacation);
@@ -36,6 +31,5 @@ public class CreateVacationDocumentsDelegate implements JavaDelegate {
             delegateExecution.setVariable(SENDING_STATUS, DECLINE);
             throw new IllegalStateException("Vacation is missing");
         }
-        LOGGER.info(delegateExecution.getVariable(SENDING_STATUS).toString());
     }
 }
